aea8eaae7b42658bd13a49b76968cd5d6b0f7de1
pedropacheco95 <pedropacheco95@gmail.com>
2022-09-15 10:57:59 +0100

    Correction of ridiculous error

--- padel_league/modules/products.py
+++ padel_league/modules/products.py
@@ -1,4 +1,3 @@
-from turtle import pen
 from flask import Blueprint, flash, g, redirect, render_template, request, session, url_for
 from werkzeug.security import check_password_hash, generate_password_hash
 import os
