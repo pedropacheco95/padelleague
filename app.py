60ca4cd01189bdf9fa83b8df7dc28c07b1d19c4b
Pedro Pacheco <pedro.pacheco@tekever.com>
2025-09-02 19:27:33 +0100

    Correct bugs, and improve image calling

--- app.py
+++ app.py
@@ -2,13 +2,3 @@ import padel_league as app
 
 run_app = app.create_app()
 run_app.run(host="0.0.0.0", port=80)
-
-"""
-
-Se for preciso correr sem 'flask run' talvez seja necessario ter isto:
-run_app.run(debug=True) 
-
-(Talvez dentro dum if __name__="__main__")
-
-"""
-
