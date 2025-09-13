c111db93f066e0d8e618c099a53df0c443c71835
Pedro Pacheco <pedro.pacheco@tekever.com>
2025-09-13 13:08:59 +0100

    Bug fix with imports

---
+++ padel_league/models/__init__.py
@@ -0,0 +1,47 @@
+from .leagues import League
+from .editions import Edition
+from .divisions import Division
+from .users import User
+from .players import Player
+from .matches import Match
+from .news import News
+from .registrations import Registration
+from .products import Product
+from .product_images import ProductImage
+from .product_attributes import ProductAttribute
+from .product_attribute_values import ProductAttributeValue
+from .orders import Order
+from .order_lines import OrderLine
+from .backend_apps import Backend_App
+from .sponsors import Sponsor
+from .sponsor_clicks import SponsorClick
+from .Association_PlayerDivision import Association_PlayerDivision
+from .Association_PlayerMatch import Association_PlayerMatch
+from .Association_ProductProductAttribute import Association_ProductProductAttribute
+from .Association_ProductProductAttributeValue import (
+    Association_ProductProductAttributeValue,
+)
+
+__all__ = [
+    "Backend_App",
+    "League",
+    "Edition",
+    "Division",
+    "Player",
+    "Match",
+    "News",
+    "Registration",
+    "Product",
+    "User",
+    "ProductImage",
+    "ProductAttribute",
+    "ProductAttributeValue",
+    "Order",
+    "OrderLine",
+    "Sponsor",
+    "SponsorClick",
+    "Association_PlayerDivision",
+    "Association_PlayerMatch",
+    "Association_ProductProductAttribute",
+    "Association_ProductProductAttributeValue",
+]
