a04e83fa0739fc2211c15b9b515af42b24cb6ce4
Pedro Pacheco <pedropacheco95@gmail.com>
2026-03-01 12:57:00 +0000

    Changes to api and imports

--- padel_league/modules/__init__.py
+++ padel_league/modules/__init__.py
@@ -18,13 +18,7 @@ from . import (
     startup,
     chatbot_api,
 )
-from .frontend_api.v1 import auth as api_v1_auth
-from .frontend_api.v1 import main as api_v1_main
-from .frontend_api.v1 import matches as api_v1_matches
-from .frontend_api.v1 import players as api_v1_players
-from .frontend_api.v1 import shuffle_tournament as api_v1_shuffle_tournament
-from .frontend_api.v1 import tournaments as api_v1_tournaments
-from .frontend_api.v1 import divisions as api_v1_divisions
+from .frontend_api.v1 import register_api_blueprints as register_v1_blueprints
 
 
 # Register Blueprints
@@ -47,13 +41,7 @@ def register_blueprints(app):
     app.register_blueprint(sponsors.bp)
     app.register_blueprint(chatbot_api.bp)
 
-    app.register_blueprint(api_v1_auth.bp)
-    app.register_blueprint(api_v1_main.bp)
-    app.register_blueprint(api_v1_matches.bp)
-    app.register_blueprint(api_v1_players.bp)
-    app.register_blueprint(api_v1_shuffle_tournament.bp)
-    app.register_blueprint(api_v1_tournaments.bp)
-    app.register_blueprint(api_v1_divisions.bp)
+    register_v1_blueprints(app)
 
     return True
 
